58e1b6d7956467163c9971ca79346d54a7f09b12
Beatriz Maia <prbeatrizmaia@gmail.com>
2023-03-06 23:44:53 -0300

    ..

--- livraria/src/componentes/Banner/index.js
+++ livraria/src/componentes/Banner/index.js
@@ -3,7 +3,7 @@ import './index.css'
 function Banner() {
     return(
         <header className="banner">
-        <img src="/imagens/banner.png" alt="O banner principal da página da livraria" />
+        <img src="./imagens/banner.png" alt="O banner principal da página da livraria" />
         </header>
     )
 }
