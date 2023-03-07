4845dfc8b6213526b8cb58a497a6774237078028
Beatriz Maia <prbeatrizmaia@gmail.com>
2023-03-07 00:23:25 -0300

    .

--- livraria/src/App.js
+++ livraria/src/App.js
@@ -5,71 +5,79 @@ import Rodape from "./componentes/Footer";
 import Form from "./componentes/Form";
 
 function App() {
+
   const categorias = [
-    {
-      nome: "Programação",
-      corPrimaria: "#57C278",
-      corSecundaria: "#D9F7E9",
+    { 
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSegundaria: '#D9F7E9' 
     },
-    {
-      nome: "Front-End",
-      corPrimaria: "#82CFFA",
-      corSecundaria: "#E8F8FF",
+    { 
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSegundaria: '#E8F8FF' 
     },
-    {
-      nome: "Data Science",
-      corPrimaria: "#A6D157",
-      corSecundaria: "#F0F8E2",
+    { 
+      nome: 'Data Sciene',
+      corPrimaria: '#A6D157',
+      corSegundaria: '#F0F8E2' 
     },
-    {
-      nome: "Devops",
-      corPrimaria: "#E06B69",
-      corSecundaria: "#FDE7E8",
+    { 
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSegundaria: '#FDE7E8' 
     },
-    {
-      nome: "UX e Design",
-      corPrimaria: "#DB6EBF",
-      corSecundaria: "#FAE9F5",
+    { 
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSegundaria: '#FAE9FS' 
     },
-    {
-      nome: "Mobile",
-      corPrimaria: "#FFBA05",
-      corSecundaria: "#FFF5D9",
+    { 
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSegundaria: '#FFF5D6' 
     },
-    {
-      nome: "Inovação e Gestão",
-      corPrimaria: "#FF8A29",
-      corSecundaria: "#FFEEDF",
+    { 
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSegundaria: '#FFEEDF' 
     },
   ];
-
-  const [livros, setLivros] = useState([]);
-
-  const NovoLivroAdicionado = (livro) => {
-    console.log(livro);
-    setLivros([...livros, livro]);
+  
+  const [livros, setLivros] = useState([])
+  
+  const NovoLivroCadastrado = (livro) => {
+    console.log(livro)
+    setLivros([...livros, livro])
   };
 
-  return (
-    <div className="App">
-      <Banner />
-      <Form
-        categorias={categorias.map((categoria) => categoria.nome)}
-        LivroCadastrado={(livro) => NovoLivroAdicionado(livro)}
-      />
-     
-      {categorias.map((categoria) => (
-        <Category
+  let categoriasL = [];
+
+  categorias.forEach(categoria => {
+    if (livros.filter(livro => livro.categoria === categoria.nome).length > 0) {
+      categoriasL.push(categoria);
+    }
+  });
+  
+    return (
+      <div className="App">
+        <Banner/>
+        <Form
+        categorias ={categorias.map((categoria) => categoria.nome)}
+        LivroCadastrado = {(livro) => NovoLivroCadastrado(livro)}
+        />
+        {categoriasL.map((categoria) => (
+          <Category 
           key={categoria.nome}
           nome={categoria.nome}
           corPrimaria={categoria.corPrimaria}
-          corSecundaria={categoria.corSecundaria}
+          corSegundaria={categoria.corSegundaria}
           livros={livros.filter((livro) => livro.categoria === categoria.nome)}
-        />
-      ))}
-      <Rodape/>
-    </div>
-  );
-}
-
-export default App;
+          />
+        ))}
+          <Rodape/>
+      </div>
+    );
+  }
+  
+  export default App;
