ef6699abe6bbe954b15fbdb1dee5beaec5d39dfe
Beatriz Maia <prbeatrizmaia@gmail.com>
2023-03-06 22:05:54 -0300

    .

--- livraria/src/componentes/Footer/index.js
+++ livraria/src/componentes/Footer/index.js
@@ -4,9 +4,9 @@ const Rodape = (props) => {
     return (
         <footer className='rodape'>
             <div className='redes-sociais'>
-            <a href="#"><img src='/imagens/fb.png'/></a>
-            <a href="#"><img src='/imagens/tw.png'/></a>
-            <a href="#"><img src='/imagens/ig.png'/></a>
+            <a href="#"><img src='./imagens/fb.png'/></a>
+            <a href="#"><img src='./imagens/tw.png'/></a>
+            <a href="#"><img src='./imagens/ig.png'/></a>
             </div>
 
             <p>Desenvolvido por autor.</p>
